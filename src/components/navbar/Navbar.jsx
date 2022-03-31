6df14d45cb9903fc53e0a9ff857336093a146953
jainil jaha <jainiljaha@gmail.com>
2022-03-31 06:06:23 -0400

    Navbar completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
+import logo from '../../assets/logo_il.jpg';
 import './navbar.css';
 
+const Menu = () => (
+    <>
+    <p><a href='#home'>Home</a></p>
+    <p><a href='#wgpt3'>Developer Roadmap</a></p>
+    <p><a href='#possibility'>AI</a></p>
+    <p><a href='#home'>Cryptocurrency</a></p>
+    <p><a href='#features'>Robotics</a></p>
+    <p><a href='#home'>Networks & security</a></p>
+    <p><a href='#blog'>about us</a></p>
+    </>
+)
+
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+        <div className='gpt3__navbar-links'>
+         <div className='gpt3__navbar-links_logo'>
+             <img src={logo} alt="logo"/>
+         </div>
+         <div className='gpt3__navbar-links_container'>
+             <Menu/>
+         </div>
+        </div>
+        <div className='gpt3__navbar-sign'>
+           <button type='button'>Handbook</button>
+        </div>
+        <div className='gpt3__navbar-menu'>
+            {toggleMenu
+            ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+            : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+            <div className='gpt3__navbar-menu_container scale-up-center'>
+                <div className='gpt3__navbar-menu_container-links'>
+                <Menu/>
+                </div>
+            </div>
+        )}
+        </div>
+    </div>
   )
 }
 
